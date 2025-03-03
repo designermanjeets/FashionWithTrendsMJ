d74406f79096ad1b5266ceb4c277a9c7f7e5baa7
designermanjeets@gmail.com <designermanjeets@gmail.com>
2025-03-03 22:17:22 +0530

    Headers Store ID

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -39,6 +39,13 @@ export class AuthInterceptor implements HttpInterceptor {
       req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
+          'store_id': '22',
+        },
+      });
+    } else {
+      req = req.clone({
+        setHeaders: {
+          'store_id': '22',
         },
       });
     }
